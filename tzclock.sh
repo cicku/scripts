7cc21fba84d1a3dc1d06c710e258fd800c30a672
FGK <f6k@opmbx.org>
2015-07-25 03:43:56 +0200

    little improvement

--- tzclock.sh
+++ tzclock.sh
@@ -1,20 +1,17 @@
-#!/bin/ksh
+#!/bin/sh
 
-DOW=$( date +%a )
-SHORT=$( date +'%d %b' )
 if [ -n "${IN_RPECHO}" ] ; then
         printf " "
 fi
-printf "%-8.8s% -7.7s%-9.9s %-6.6s\n" "$DOW" "Eastern" " Acapulco" "Paris"
+printf "%-7.7s%-7.7s%-7.7s%-6.6s\n" "Acap." "East." "Paris" "Japan"
 if [ -n "${IN_RPECHO}" ] ; then
         printf " "
 fi
-printf "%-7.7s " "$SHORT"
-zdump Canada/Eastern Mexico/General Europe/Paris | \
+zdump Mexico/General Canada/Eastern  Europe/Paris Japan | \
         awk '{print $5}' | \
         cut -c1-5 | \
         while read TIME ; do
-        printf " %-6.6s " "$TIME"
+        printf "%-6.6s " "$TIME"
 
 done
 
