edcaf8761ef6c7ebaf0aaac5d777e14b83eea0ac
FGK <f6k@opmbx.org>
2015-07-28 02:04:44 +0200

    new script in database

---
+++ lock.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ 
+# Copied from https://github.com/meskarune/i3lock-fancy
+
+IMAGE=/tmp/lock.png
+
+scrot $IMAGE
+convert $IMAGE -level 0%,100%,0.8 -blur 0x2 -fill "#fcefba" -pointsize 26 -gravity center -annotate +0+100 'Type password to unlock' - | composite -gravity center lock.png - $IMAGE
+feh -F $IMAGE
+#i3lock -i $IMAGE
+rm $IMAGE
